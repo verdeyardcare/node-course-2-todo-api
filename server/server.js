d47f38879f3ef96f94c453b8dec6b1930de02947
verdeyardcare <30375500+verdeyardcare@users.noreply.github.com>
2017-10-14 11:43:35 -0700

    Test POST /todos route

---
+++ server/server.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todos');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+});
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Started on port 3000')
+});
+
+module.exports = {app};
+
+
+
+
+
+
+
+
+//
+//var newTodo = new Todo({
+//    text: 'Cook dinner'
+//});
+//
+//newTodo.save().then((doc) => {
+//    console.log('Saved todo', doc);
+//}, (e) => {
+//    console.log('Unable to save todo')
+//});
+//
+//var otherTodo = new Todo({
+//    text: '   Edit this video   '
+////    text: 'Feed the cat',
+////    completed: true,
+////    completedAt: 123
+//});
+//
+//otherTodo.save().then((doc) => {
+//    console.log(JSON.stringify(doc, undefined, 2));
+//}, (e) => {
+//    console.log('Unable to save', e)
+//});
+
+
+
