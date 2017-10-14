73d4725e40d4d9946ca9c8c82eabe400ab9c0a26
verdeyardcare <30375500+verdeyardcare@users.noreply.github.com>
2017-10-14 14:51:00 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var{ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todos');
@@ -27,6 +28,23 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     })
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+   if(!ObjectID.isValid(id)){
+       return res.status(404).send();
+   }
+    
+    Todo.findByID(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        
+        res.send({todo})
+    }).catch((e) => {
+        res.status(400).send();
+    })  
 });
 
 app.listen(3000, () => {
